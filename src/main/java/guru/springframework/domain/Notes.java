d9b0c73c6ec02b0d310838bdfe3920345ee2e72f
Jenny <yz2483@att.com>
2019-05-13 14:45:08 -0700

    first commit

---
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -0,0 +1,45 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created by Jenny on 6/13/18.
+ */
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+//  @Column(columnDefinition="BLOB")
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
